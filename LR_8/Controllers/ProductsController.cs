f03a53cdc3472661237aa44e918793823af5ddab
mV22110702 <92586731+mV22110702@users.noreply.github.com>
2023-10-24 20:43:28 +0300

    Добавьте файлы проекта.

---
+++ LR_8/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using LR_8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LR_8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var orangeProduct = new Product()
+            {
+                Id = 0,
+                Name = "Orange",
+                Price = 10,
+                CreatedDate = DateTime.Today.AddDays(-3)
+            };
+            var cherryProduct = new Product()
+            {
+                Id = 1,
+                Name = "Cherry",
+                Price = 15,
+                CreatedDate = DateTime.Today.AddDays(-2)
+            };
+            var watermelonProduct = new Product()
+            {
+                Id = 2,
+                Name = "Watermelon",
+                Price = 17,
+                CreatedDate = DateTime.Today.AddDays(-1)
+            };
+            var products = new List<Product>() { orangeProduct, cherryProduct, watermelonProduct };
+            return View(products);
+        }
+    }
+}
---
+++ LR_8/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LR_8.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
